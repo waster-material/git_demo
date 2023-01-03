2e978161f3e6fb9085bc893af69fa8bb9277cba6
lcy <3517624381@qq.com>
2023-01-03 20:28:12 +0800

    master commit

--- test.java
+++ test.java
@@ -1,9 +1,16 @@
 public class test{       1111111
-public class test{
+public class test{            
    public static void main(String[] args){
         System.out.println("hello world"+'\n');
    public static void main(String[] args){
         System.out.println("hello world"+'\n');
 
   }
+}
+
+#include<stdio.h>
+int main(){
+    printf("hello world\n"); master test
+    printf("hello world\n");
+    
 }
